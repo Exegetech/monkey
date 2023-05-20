3c9c03e6dd5cc5f7fd2cbdc5d9364bc480d0e91b
Exegetech <exegetech@protonmail.ch>
2023-05-20 17:08:46 -0400

    Update parser print

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -147,3 +147,30 @@ func TestReturnStatement(t *testing.T) {
     }
   }
 }
+
+// func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+//   ident, ok := exp.(*ast.Identifier)
+//   if !ok {
+//     t.Errorf("exp not *ast.Identifier, got = %T", exp)
+//     return false
+//   }
+
+//   if ident.Value != value {
+//     t.Errorf("ident.Value not %s. got = %s", value, ident.Value)
+//     return false
+//   }
+
+//   if ident.TokenLiteral() != value {
+//     t.Errorf("ident.TokenLiteral() not %s. got = %s", value, ident.Value)
+//     return false
+//   }
+
+//   return true
+// }
+
+// func testLiteralExpression(t *testing.T, exp ast.Expression, expected interfade{}) bool {
+//   switch v := expected.(type) {
+//   case int:
+//     return testIntegerLiteral(
+// }
+
