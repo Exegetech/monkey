1c56fac66dc6f5cb9e8eb140ec40cd9d4ea622a4
Exegetech <exegetech@protonmail.ch>
2023-05-20 17:55:50 -0400

    Parse prefix expression

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -47,6 +47,8 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	return p
 }
@@ -181,9 +183,15 @@ func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
 }
 
+func (p *Parser) noPrefixParseFnError(tokenType token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", tokenType)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 
@@ -214,3 +222,17 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 
 	return lit
 }
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+  expression := &ast.PrefixExpression{
+		Token: p.curToken,
+    Operator: p.curToken.Literal,
+	}
+
+  p.nextToken()
+
+  expression.Right = p.parseExpression(PREFIX)
+
+	return expression
+}
+
